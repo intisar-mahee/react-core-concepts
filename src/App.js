15d95d5387ea82eb7c66206a37f7d220516e0d2a
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-10-03 13:10:12 +0600

    first

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person1 = {
+    name: "Dr. Mahfuz",
+    job: "Singer"
+    }
+
+    var person2 = {
+      name: "Eva",
+      job: "Stupid"
+      }
+
+      var style={
+        color: 'yellow',
+        backgroundColor: 'red'
+      }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,9 +24,12 @@ function App() {
         <p>
           Edit Done  <code>src/App.js</code> and save to reload.
         </p>
-        <p>Getting Started wth REACT</p>
+        <p className="style">Getting Started wth REACT</p>
         <p>Feeling Excited</p>
-        <h1>Intisar Mahee</h1>
+        <h1 style={{color: 'yellow',
+        backgroundColor: 'hotpink'}}>Intisar Mahee</h1>
+        <h2>My Birth Year {1900+95}</h2>
+  <h1>my heading: {person1.name + " " + person2.job}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
