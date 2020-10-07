0261e405d36954c5714f2b11a640bd2631097215
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-10-07 19:20:03 +0600

    sec

--- src/App.js
+++ src/App.js
@@ -37,9 +37,13 @@ function App() {
 
 function Counter(){
   const[count, setCount] = useState(20);
+  
+  
   return(
     <div>
       <h1>Count: {count}</h1>
+      <button onClick={()=>setCount(count+1)}>Increase</button>
+      <button onClick={()=>setCount(count-1)}>Decrease</button>
     </div>
   )
 }
